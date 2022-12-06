e34852f80e470314a9b40856e429c9c230712bb0
Daniel Nelson <afredknot@gmail.com>
2022-12-05 20:25:23 -0500

    updating the index model

--- models/index.js
+++ models/index.js
@@ -5,10 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
 
+Product.belongToMany(Tag, {
+  through:{
+    model: ProductTag,
+  },
+  as: 'tag_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
+
+Tag.belongToMany(Product, {
+ through:{
+  model: ProductTag
+ },
+ as: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
